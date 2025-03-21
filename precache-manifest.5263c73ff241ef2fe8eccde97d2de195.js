8e381129e6b3c1eda8fe4edc0ce1ba3a7d5f8af4
Antonio Escamilla <7999008+AntonioEscamilla@users.noreply.github.com>
2025-03-21 18:19:35 -0500

    Publicación en GitHub Pages

---
+++ precache-manifest.5263c73ff241ef2fe8eccde97d2de195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901d848dac99f1d5caaa08417a5b265f",
+    "url": "/perceptron-visualization/index.html"
+  },
+  {
+    "revision": "81dce7819d637d735e10",
+    "url": "/perceptron-visualization/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "abf5ac9533b5e2397fe5",
+    "url": "/perceptron-visualization/static/js/2.8153b4b1.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/perceptron-visualization/static/js/2.8153b4b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e094f414cf3478fe6bf2",
+    "url": "/perceptron-visualization/static/js/3.3ce5a02c.chunk.js"
+  },
+  {
+    "revision": "81dce7819d637d735e10",
+    "url": "/perceptron-visualization/static/js/main.d878cb76.chunk.js"
+  },
+  {
+    "revision": "3b81c02612a80d5853d7",
+    "url": "/perceptron-visualization/static/js/runtime-main.3d0d56d6.js"
+  }
+]);
