7ad80db6d6159fb2e7d1036c717f4b287a14784e
Antonio Escamilla <7999008+AntonioEscamilla@users.noreply.github.com>
2025-03-22 13:55:54 -0500

    component code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import PerceptronNetworkAnimation from './components/PerceptronNetworkAnimation'
+
+function App() {
+  return (
+    <div className="App">
+      <PerceptronNetworkAnimation />
+    </div>
+  )
+}
+
+export default App
